01b38cb49b275d85cd5ed1870e1218ff939d786f
Nusk-Rbb <nuskrbb1741dn@gmail.com>
2025-01-17 08:19:14 +0900

    :sparkles: All Done

--- frontend/js/script.js
+++ frontend/js/script.js
@@ -1,3 +1,61 @@
+let token = localStorage.getItem('token');
+const authContainer = document.getElementById('auth-container');
+const mainContainer = document.getElementById('main-container');
+if (token){
+    authContainer.style.display = "none";
+    mainContainer.style.display = "block";
+} else {
+    authContainer.style.display = "block";
+    mainContainer.style.display = "none";
+}
+document.getElementById('signupBtn').addEventListener('click', () => {
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+
+    fetch('http://172.18.0.3:8080/signup', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ email, password }),
+    })
+    .then(response => response.json())
+    .then(data => {
+        localStorage.setItem('token', data.token);
+        document.getElementById('auth-message').textContent = "サインアップ完了";
+        authContainer.style.display = "none";
+        mainContainer.style.display = "block";
+    })
+    .catch(error => {
+        console.error('Error:', error);
+        document.getElementById('auth-message').textContent = "サインアップに失敗";
+    });
+});
+
+document.getElementById('loginBtn').addEventListener('click', () => {
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+    fetch('http://172.18.0.3:8080/login', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ email, password }),
+    })
+    .then(response => response.json())
+    .then(data => {
+        localStorage.setItem('token', data.token);
+        document.getElementById('auth-message').textContent = "ログイン完了";
+        authContainer.style.display = "none";
+        mainContainer.style.display = "block";
+    })
+    .catch(error => {
+        console.error('Error:', error);
+        document.getElementById('auth-message').textContent = "ログインに失敗";
+    });
+});
+
+
 document.getElementById('getLocationBtn').addEventListener('click', () => {
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(success, error);
@@ -6,11 +64,19 @@ document.getElementById('getLocationBtn').addEventListener('click', () => {
     }
 });
 
+document.getElementById('putLocationBtn').addEventListener('click', () => {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(putUserLocation, error);
+    } else {
+        document.getElementById('status').textContent = "このブラウザは位置情報をサポートしていません。";
+    }
+})
+
 function success(position) {
     const latitude = position.coords.latitude;
     const longitude = position.coords.longitude;
-    const request_body = JSON.stringify({ latitude, longitude });
-    console.log(request_body);
+    const human_sensor = true; 
+    const light_sensor = true;
     document.getElementById('status').textContent = "位置情報を送信中...";
 
     fetch('http://172.18.0.3:8080/check-location', {
@@ -18,13 +84,58 @@ function success(position) {
         headers: {
             'Content-Type': 'application/json',
         },
-        body: request_body
+        body: JSON.stringify({ latitude, longitude, human_sensor, light_sensor}),
     })
     .then(response => {
-        console.log(response);
+        if(response.ok){
+            document.getElementById('status').textContent = "位置情報を更新しました。"
+        }
     })
-    .then(data => {
-        console.log(data);
+    .catch(error => {
+        console.error('Error:', error);
+        document.getElementById('status').textContent = "判定に失敗しました。";
+    });
+}
+
+function putUserLocation(position) {
+    const latitude = position.coords.latitude;
+    const longitude = position.coords.longitude;
+    document.getElementById('status').textContent = "位置情報を送信中...";
+
+    fetch('http://172.18.0.3:8080/put-user-location', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ latitude, longitude }),
+    })
+    .then(response => {
+        if(response.ok){
+            document.getElementById('status').textContent = "位置情報を更新しました。"
+        }
+    })
+    .catch(error => {
+        console.error('Error:', error);
+        document.getElementById('status').textContent = "判定に失敗しました。";
+    });
+}
+
+function putHomeLocation(position) {
+    const latitude = position.coords.latitude;
+    const longitude = position.coords.longitude;
+    document.getElementById('status').textContent = "位置情報を送信中...";
+
+    fetch('http://172.18.0.3:8080/put-home-location', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ latitude, longitude }),
+    })
+    .then(response => {
+        if(response.ok){
+            document.getElementById('status').textContent = "位置情報を更新しました。"
+        }
     })
     .catch(error => {
         console.error('Error:', error);
@@ -35,3 +146,37 @@ function success(position) {
 function error() {
     document.getElementById('status').textContent = "位置情報の取得に失敗しました。";
 }
+
+document.getElementById('changeEmailBtn').addEventListener('click', () => {
+    document.getElementById('changeEmailForm').style.display = "block";
+});
+
+document.getElementById('submitEmailBtn').addEventListener('click', () => {
+    const newEmail = document.getElementById('newEmail').value;
+
+    fetch('http://172.18.0.3:8080/change-email', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': localStorage.getItem('token'),
+        },
+        body: JSON.stringify({email:newEmail}),
+    })
+    .then(response => response.json())
+    .then(data => {
+        console.log(data);
+        document.getElementById('email-change-message').textContent = "メールアドレスを変更しました。";
+        localStorage.removeItem('token');
+        document.getElementById('auth-container').style.display = "block";
+        document.getElementById('main-container').style.display = "none";
+        
+    })
+    .catch(error => {
+        console.error('Error:', error);
+        document.getElementById('email-change-message').textContent = "メールアドレス変更に失敗しました。";
+    });
+})
+document.getElementById('logoutBtn').addEventListener('click', () => {
+    localStorage.removeItem('token');
+    window.location.reload();
+})
